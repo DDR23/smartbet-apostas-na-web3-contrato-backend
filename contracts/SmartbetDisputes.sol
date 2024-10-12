58fc6080e6d80d47323db0f620bfef1a6cf8aed5
DDR23 <roboreserva.andre@gmail.com>
2024-10-11 23:03:13 -0300

    save

--- contracts/SmartbetDisputes.sol
+++ contracts/SmartbetDisputes.sol
@@ -1,8 +1,13 @@
+/**
+ *Submitted for verification at amoy.polygonscan.com on 2024-10-12
+*/
+
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
-contract SmartbetDisputes {
+contract SmartbetContract {
   address public owner;
+  uint256 private immutable deploymentId;
   uint256 private constant FEE_PERCENTAGE = 1000;
 
   modifier onlyOwner() {
@@ -11,18 +16,18 @@ contract SmartbetDisputes {
   }
 
   struct Dispute {
-    string disputeName;
-    string disputeWallpaper;
-    string disputeCandidate1;
-    string disputeCandidateImage1;
-    uint256 disputeCandidateBet1;
-    string disputeCandidate2;
-    string disputeCandidateImage2;
-    uint256 disputeCandidateBet2;
-    uint8 disputeWinner;
-    uint256 disputeNetPrize;
-    uint256 disputeFee;
-    bool disputeStatus;
+    string Name;
+    string Wallpaper;
+    string Candidate1;
+    string CandidateImage1;
+    uint256 CandidateBet1;
+    string Candidate2;
+    string CandidateImage2;
+    uint256 CandidateBet2;
+    uint8 Winner;
+    uint256 NetPrize;
+    uint256 Fee;
+    bool Status;
   }
 
   mapping(uint256 => Dispute) public disputes;
@@ -42,30 +47,31 @@ contract SmartbetDisputes {
 
   constructor() {
     owner = msg.sender;
+    deploymentId = block.timestamp;
   }
 
   function createDispute(
-    string memory _disputeName,
-    string memory _disputeWallpaper,
-    string memory _disputeCandidate1,
-    string memory _disputeCandidateImage1,
-    string memory _disputeCandidate2,
-    string memory _disputeCandidateImage2
+    string memory _Name,
+    string memory _Wallpaper,
+    string memory _Candidate1,
+    string memory _CandidateImage1,
+    string memory _Candidate2,
+    string memory _CandidateImage2
   ) public onlyOwner {
     currentDisputeId++;
     disputes[currentDisputeId] = Dispute({
-      disputeName: _disputeName,
-      disputeWallpaper: _disputeWallpaper,
-      disputeCandidate1: _disputeCandidate1,
-      disputeCandidateImage1: _disputeCandidateImage1,
-      disputeCandidate2: _disputeCandidate2,
-      disputeCandidateImage2: _disputeCandidateImage2,
-      disputeCandidateBet1: 0,
-      disputeCandidateBet2: 0,
-      disputeWinner: 0,
-      disputeNetPrize: 0,
-      disputeFee: 0,
-      disputeStatus: true
+      Name: _Name,
+      Wallpaper: _Wallpaper,
+      Candidate1: _Candidate1,
+      CandidateImage1: _CandidateImage1,
+      Candidate2: _Candidate2,
+      CandidateImage2: _CandidateImage2,
+      CandidateBet1: 0,
+      CandidateBet2: 0,
+      Winner: 0,
+      NetPrize: 0,
+      Fee: 0,
+      Status: true
     });
     disputeIds.push(currentDisputeId);
   }
@@ -76,7 +82,7 @@ contract SmartbetDisputes {
 
   function createBet(uint256 _disputeId, uint8 _candidateNumber) public payable {
     require(msg.value > 0, "Bet value must be greater than zero");
-    require(disputes[_disputeId].disputeWinner == 0, "Dispute already finished");
+    require(disputes[_disputeId].Winner == 0, "Dispute already finished");
     require(walletBets[msg.sender][_disputeId].amount == 0, "Already bet on this dispute");
 
     Dispute storage dispute = disputes[_disputeId];
@@ -85,13 +91,13 @@ contract SmartbetDisputes {
     uint256 netBet = msg.value - fee;
 
     if (_candidateNumber == 1) {
-      dispute.disputeCandidateBet1 += netBet;
+      dispute.CandidateBet1 += netBet;
     } else {
-      dispute.disputeCandidateBet2 += netBet;
+      dispute.CandidateBet2 += netBet;
     }
 
-    dispute.disputeFee += fee;
-    dispute.disputeNetPrize += netBet;
+    dispute.Fee += fee;
+    dispute.NetPrize += netBet;
 
     Bet memory newBet = Bet({
       disputeId: _disputeId,
@@ -105,29 +111,28 @@ contract SmartbetDisputes {
     walletDisputeIds[msg.sender].push(_disputeId);
   }
 
-  function getAllWalletBets(address _wallet) public view returns (uint256[] memory) {
-    return walletDisputeIds[_wallet];
-  }
-
   function getBetDetails(address _wallet, uint256 _disputeId) public view returns (Bet memory) {
     return walletBets[_wallet][_disputeId];
   }
 
+  function getAllWalletBets(address _wallet) public view returns (uint256[] memory) {
+    return walletDisputeIds[_wallet];
+  }
+
+  function toggleStatus(uint256 _disputeId, bool _status) public onlyOwner {
+    Dispute storage dispute = disputes[_disputeId];
+    dispute.Status = _status;
+  }
+
   function finishDispute(uint256 _disputeId, uint8 _winner) public onlyOwner {
-    require(disputes[_disputeId].disputeWinner == 0, "Dispute already finished");
+    require(disputes[_disputeId].Winner == 0, "Dispute already finished");
     require(_winner == 1 || _winner == 2, "Invalid winner");
 
     Dispute storage dispute = disputes[_disputeId];
-    dispute.disputeWinner = _winner;
+    dispute.Winner = _winner;
 
-    uint256 feeToTransfer = dispute.disputeFee;
+    uint256 feeToTransfer = dispute.Fee;
     payable(owner).transfer(feeToTransfer);
-  }
-
-  function toggleDisputeStatus(uint256 _disputeId, bool _status) public onlyOwner {
-    require(_status == true || _status == false, "Status must be true or false");
-    Dispute storage dispute = disputes[_disputeId];
-    dispute.disputeStatus = _status;
   }
 
   function claimPrize(uint256 _disputeId) public {
@@ -135,16 +140,16 @@ contract SmartbetDisputes {
     Dispute storage dispute = disputes[_disputeId];
 
     require(bet.amount > 0, "No bet found");
-    require(dispute.disputeWinner != 0, "Dispute not finished");
+    require(dispute.Winner != 0, "Dispute not finished");
     require(!bet.collected, "Bet already collected");
-    require(bet.candidateNumber == dispute.disputeWinner, "You didn't bet on the winning candidate");
+    require(bet.candidateNumber == dispute.Winner, "You didn't bet on the winning candidate");
 
-    uint256 winningCandidateBet = dispute.disputeWinner == 1
-      ? dispute.disputeCandidateBet1
-      : dispute.disputeCandidateBet2;
-    uint256 losingCandidateBet = dispute.disputeWinner == 1
-      ? dispute.disputeCandidateBet2
-      : dispute.disputeCandidateBet1;
+    uint256 winningCandidateBet = dispute.Winner == 1
+      ? dispute.CandidateBet1
+      : dispute.CandidateBet2;
+    uint256 losingCandidateBet = dispute.Winner == 1
+      ? dispute.CandidateBet2
+      : dispute.CandidateBet1;
 
     uint256 ratio = (bet.amount * 1e18) / winningCandidateBet;
     uint256 individualPrize = ((winningCandidateBet + losingCandidateBet) * ratio) / 1e18;
